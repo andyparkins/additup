4a90dd6ef4b2f5b8d0cf77229820a9821823262b
Andy Parkins <andyparkins@gmail.com>
2011-04-18 22:20:22 +0100

    Add a crypto module for signatures and hashes -- empty as yet

---
+++ src/lib/crypto.cc
@@ -0,0 +1,56 @@
+// ----------------------------------------------------------------------------
+// Project: bitcoin
+/// @file   crypto.cc
+/// @author Andy Parkins
+//
+// Version Control
+//    $Author$
+//      $Date$
+//        $Id$
+//
+// Legal
+//    Copyright 2011  Andy Parkins
+//
+// ----------------------------------------------------------------------------
+
+// Module include
+#include "crypto.h"
+
+// -------------- Includes
+// --- C
+// --- C++
+// --- Qt
+// --- OS
+// --- Project libs
+// --- Project
+
+
+// -------------- Namespace
+
+
+// -------------- Module Globals
+
+
+// -------------- World Globals (need "extern"s in header)
+
+
+// -------------- Template instantiations
+
+
+// -------------- Class declarations
+
+
+// -------------- Function definitions
+
+
+#ifdef UNITTEST
+#include <iostream>
+
+// -------------- main()
+
+int main( int argc, char *argv[] )
+{
+	return 0;
+}
+#endif
+
---
+++ src/lib/crypto.h
@@ -0,0 +1,76 @@
+// ----------------------------------------------------------------------------
+// Project: bitcoin
+/// @file   crypto.h
+/// @author Andy Parkins
+//
+// Version Control
+//    $Author$
+//      $Date$
+//        $Id$
+//
+// Legal
+//    Copyright 2011  Andy Parkins
+//
+// ----------------------------------------------------------------------------
+
+// Catch multiple includes
+#ifndef CRYPTO_H
+#define CRYPTO_H
+
+// -------------- Includes
+// --- C
+// --- C++
+// --- OS
+// --- Project
+
+
+// -------------- Namespace
+	// --- Imported namespaces
+	using namespace std;
+
+
+// -------------- Defines
+// General
+// Project
+
+
+// -------------- Constants
+
+
+// -------------- Typedefs (pre-structure)
+
+
+// -------------- Enumerations
+
+
+// -------------- Structures/Unions
+
+
+// -------------- Typedefs (post-structure)
+
+
+// -------------- Class pre-declarations
+
+
+// -------------- Function pre-class prototypes
+
+
+// -------------- Class declarations
+
+
+// -------------- Constants
+
+
+// -------------- Inline Functions
+
+
+// -------------- Function prototypes
+
+
+// -------------- Template instantiations
+
+
+// -------------- World globals ("extern"s only)
+
+// End of conditional compilation
+#endif
