fad1725790dbb8c629cc18d997b80aa57645abae
Andy Parkins <andyparkins@gmail.com>
2011-07-10 09:26:49 +0100

    Minor commentary fixes constants module

--- src/lib/constants.cc
+++ src/lib/constants.cc
@@ -29,7 +29,6 @@
 #include "blockchain.h"
 #include "messages.h"
 #include "script.h"
-#include "logstream.h"
 
 
 // -------------- Namespace
@@ -175,9 +174,6 @@ class TTestnetNetworkParameters : public TPredefinedNetworkParameters
 		message.setMerkleRoot();
 		message.setHeader();
 
-		log() << "TESTNET: ";
-		log() << message << endl;
-
 		string GenesisHash( "\x00\x00\x00\x07\x19\x95\x08\xe3"
 				"\x4a\x9f\xf8\x1e\x6e\xc0\xc4\x77"
 				"\xa4\xcc\xcf\xf2\xa4\x76\x7a\x8e"
@@ -189,8 +185,6 @@ class TTestnetNetworkParameters : public TPredefinedNetworkParameters
 		// see in the block chain
 		GenesisBlock = new TMessageBasedBlock( NULL );
 		GenesisBlock->updateFromMessage( GenesisHash, &message );
-
-		// Calculate the Merkle tree
 	}
 	const char *className() const { return "TTestnetNetworkParameters"; }
 };
@@ -220,13 +214,51 @@ class TProdnetNetworkParameters : public TPredefinedNetworkParameters
 		message.setMerkleRoot();
 		message.setHeader();
 
-		log() << "PRODNET: ";
-		log() << message << endl;
-
 		string GenesisHash( "\x00\x00\x00\x00\x00\x19\xd6\x68"
 				"\x9c\x08\x5a\xe1\x65\x83\x1e\x93"
 				"\x4f\xf7\x63\xae\x46\xa2\xa6\xc1"
 				"\x72\xb3\xf1\xb6\x0a\x8c\xe2\x6f" , 32 );
+
+		// From http://blockexplorer.com/b/0
+		// {
+		//  "hash":"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
+		//  "ver":1,
+		//  "prev_block":"0000000000000000000000000000000000000000000000000000000000000000",
+		//  "mrkl_root":"4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+		//  "time":1231006505,
+		//  "bits":486604799,
+		//  "nonce":2083236893,
+		//  "n_tx":1,
+		//  "size":285,
+		//  "tx":[
+		//    {
+		//      "hash":"4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+		//      "ver":1,
+		//      "vin_sz":1,
+		//      "vout_sz":1,
+		//      "lock_time":0,
+		//      "size":204,
+		//      "in":[
+		//        {
+		//          "prev_out":{
+		//            "hash":"0000000000000000000000000000000000000000000000000000000000000000",
+		//            "n":4294967295
+		//          },
+		//          "coinbase":"04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73"
+		//        }
+		//      ],
+		//      "out":[
+		//        {
+		//          "value":"50.00000000",
+		//          "scriptPubKey":"04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f OP_CHECKSIG"
+		//        }
+		//      ]
+		//    }
+		//  ],
+		//  "mrkl_tree":[
+		//    "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+		//  ]
+		//}
 
 		// We've created the genesis transaction, wrapped it in a
 		// TMessage_block, now we wrap that message in a TBlock, so that
@@ -234,8 +266,6 @@ class TProdnetNetworkParameters : public TPredefinedNetworkParameters
 		// see in the block chain
 		GenesisBlock = new TMessageBasedBlock( NULL );
 		GenesisBlock->updateFromMessage( GenesisHash, &message );
-
-		// Calculate the Merkle tree
 	}
 	const char *className() const { return "TProdnetNetworkParameters"; }
 };
