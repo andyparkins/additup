0ab622ccc9d5ba4b222f12493e8325697fe52b05
Andy Parkins <andyparkins@gmail.com>
2011-12-06 15:25:12 +0000

    [bitkeys] Add --version and --help to bitkeys application

--- src/bitkeys/main.cc
+++ src/bitkeys/main.cc
@@ -25,6 +25,8 @@
 // --- OS
 // --- Project libs
 #include <logstream.h>
+#include <hashtypes.h>
+#include <autoversion.h>
 // --- Project
 
 
@@ -40,11 +42,63 @@
 // -------------- Class member definitions
 
 
+// -------------- Function Definitions
+
+static void help()
+{
+	cerr << "Usage: " << endl
+		<< " bitkeys <mode specifier> <options>" << endl
+		<< "      --help     show help" << endl
+		<< "      --version  print version" << endl
+		;
+}
+
+static void version()
+{
+	cerr << "bitkeys" << VCSID_CONST << endl;
+}
+
 // ----- Main
 
 int main( int argc, char *argv[] )
 {
+	enum eApplicationMode {
+		MODE_INVALID,
+		MODE_HELP,
+		MODE_VERSION,
+		MODE_COUNT
+	} Mode = MODE_INVALID;
+
 	try {
+		unsigned int i;
+
+		for( i = 1; i < argc; i++ ) {
+			if( strcmp(argv[i], "--debug") == 0 ) {
+				TLog::instance().setLogLevel( TLog::Debug );
+				log( TLog::Debug ) << "Log level changed because --debug switch found on command line" << endl;
+			} else if( strcmp(argv[i], "--verbose") == 0 ) {
+				TLog::instance().setLogLevel( TLog::Verbose );
+				log( TLog::Verbose ) << "Log level changed because --verbose switch found on command line" << endl;
+			} else if( strcmp(argv[i], "--quiet") == 0 ) {
+				TLog::instance().setLogLevel( TLog::Warning );
+				log( TLog::Info ) << "Log level changed because --quiet switch found on command line" << endl;
+			} else if( strcmp(argv[i], "--version") == 0 ) {
+				Mode = MODE_VERSION;
+			} else if( strcmp(argv[i], "--help") == 0 ) {
+				Mode = MODE_HELP;
+			}
+		}
+
+		switch( Mode ) {
+			case MODE_HELP:
+				help();
+				break;
+			case MODE_VERSION:
+				version();
+				break;
+			default:
+				break;
+		}
 
 	} catch( exception &e ) {
 		log(TLog::Error) << typeid(e).name() << " " << e.what() << endl;
